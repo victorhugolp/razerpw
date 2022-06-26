afdcfd1bf6c81d51457f935ce3636b8fb100000e
victorhugolp <87951129+victorhugolp@users.noreply.github.com>
2022-06-26 15:53:08 -0300

    login fix 2

--- controller/login.js
+++ controller/login.js
@@ -1,3 +1,4 @@
+
 isLogado();
 
 
@@ -9,7 +10,7 @@ function isLogado(){
    if(token === "QpwL5tke4Pnpja7X4"){
       console.log("Você já está logado!")
       document.getElementById("loginButton").style.display = "none";     
-      document.getElementById("postButton").style.display = "display";
+      document.getElementById("sairButton").style.display = "display";
       buttonEntrar.style.display = "none";
       return true;
    
@@ -18,11 +19,47 @@ function isLogado(){
       console.log("Você não está logado!");
       document.getElementById("loginButton").style.display = "block";      
       document.getElementById("sairButton").style.display = "none";
-      document.getElementById("postButton").style.display = "none";
+      document.getElementById("buscarButton").style.display = "none";
       buttonEntrar.style.display = "block";
       return false;
    }
 }
+
+
+/*Modal-Entrar*/
+
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);    
+     modal.classList.add('mostrar');
+     modal.addEventListener('click', function (e) {
+        if (e.target.id == modalID || e.target.className == 'fechar' )
+        modal.classList.remove('mostrar');            
+
+});
+
+}
+
+ buttonEntrar = document.getElementById('entrarButton');
+ buttonEntrar.addEventListener('click', function () {
+     iniciaModal('modal-entrar');
+ })
+
+
+ buttonSair = document.getElementById('sairButton');
+ buttonSair.addEventListener('click', function () {
+     deslogar();
+
+     // recarregar pagina 
+     document.location.reload(true);
+ })
+
+
+
+
+document.getElementById("loginButton2").addEventListener('click', function(){
+   logar()
+});
+
 
 
 
@@ -67,6 +104,7 @@ function loginApi(email, pass){
 
     
    });
+
 }
 
 
@@ -78,7 +116,32 @@ function loginApi(email, pass){
 function store(token) {
    localStorage.setItem('token', token);
 
-} 
+}
+ 
+ /*console.log(button2);/*
+ /*Modal-login
+ 
+
+ function iniciaModal3(modal3ID){
+    const modal3 = document.getElementById(modal3ID);
+    /*console.log(modal3);
+    modal3.classList.add('mostrar3');    
+
+    modal3.addEventListener('click', function (e) {
+       if (e.target.id == modal3ID || e.target.className == 'fechar' )
+       modal3.classList.remove('mostrar3');   
+})}
+
+const login = document.getElementById('loginButton');
+
+login.addEventListener('click', function check(){
+   
+   iniciaModal3('modal-login');
+   
+} )*/
+
+
+
 /*função para validação*/
 
 function check() {
